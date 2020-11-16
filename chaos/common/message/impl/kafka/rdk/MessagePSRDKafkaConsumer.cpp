20467790fee6c0ba249ebc4348dfdb42e09cbb9a
amichelo <andrea.michelotti@lnf.infn.it>
2020-11-17 00:59:04 +0100

    fix

--- chaos/common/message/impl/kafka/rdk/MessagePSRDKafkaConsumer.cpp
+++ chaos/common/message/impl/kafka/rdk/MessagePSRDKafkaConsumer.cpp
@@ -60,33 +60,30 @@ void MessagePSRDKafkaConsumer::HandleRequest(const Connection::ErrorCodeType& er
 */
 
 MessagePSRDKafkaConsumer::~MessagePSRDKafkaConsumer() {
-  MRDDBG_<<" DESTROY CONSUMER";
-  if(rk){
+  MRDDBG_ << " DESTROY CONSUMER";
+  if (rk) {
     rd_kafka_consumer_close(rk);
 
-
-        /* Destroy the consumer */
+    /* Destroy the consumer */
     rd_kafka_destroy(rk);
   }
 }
 
-
-MessagePSRDKafkaConsumer::MessagePSRDKafkaConsumer(const std::string& gid,const std::string& defkey): chaos::common::message::MessagePSConsumer("kafka-rdk",gid,defkey),chaos::common::message::MessagePublishSubscribeBase("kafka-rdk") {
+MessagePSRDKafkaConsumer::MessagePSRDKafkaConsumer(const std::string& gid, const std::string& defkey)
+    : chaos::common::message::MessagePSConsumer("kafka-rdk", gid, defkey), chaos::common::message::MessagePublishSubscribeBase("kafka-rdk") {
 }
-int MessagePSRDKafkaConsumer::getMsgAsync(const std::string&key,const int32_t pnum){
-  
+int MessagePSRDKafkaConsumer::getMsgAsync(const std::string& key, const int32_t pnum) {
   return stats.last_err;
 }
-int MessagePSRDKafkaConsumer::setOption(const std::string& key, const std::string& value){
-  MRDDBG_<<"Setting Option:"<<key<<"="<<value;
-  return MessagePSRDKafka::setOption(key,value);
-  }
-
-int MessagePSRDKafkaConsumer::getMsgAsync(const std::string&key,uint32_t off,const int32_t pnum){
-  
-  return stats.last_err;
+int MessagePSRDKafkaConsumer::setOption(const std::string& key, const std::string& value) {
+  MRDDBG_ << "Setting Option:" << key << "=" << value;
+  return MessagePSRDKafka::setOption(key, value);
 }
 
+int MessagePSRDKafkaConsumer::getMsgAsync(const std::string& key, uint32_t off, const int32_t pnum) {
+  return stats.last_err;
+}
+/*
 static void err_cb (rd_kafka_t *rk, int err, const char *reason, void *opaque) {
         if (err == RD_KAFKA_RESP_ERR__FATAL) {
                 char errstr[512];
@@ -98,46 +95,43 @@ static void err_cb (rd_kafka_t *rk, int err, const char *reason, void *opaque) {
 
                
         }
-}
+}*/
 int MessagePSRDKafkaConsumer::applyConfiguration() {
   char ers[512];
   int  ret = 0;
-   if ((ret = MessagePSRDKafka::init(servers)) == 0) {
-    
-
-        /* If there is no previously committed offset for a partition
+  if ((ret = MessagePSRDKafka::init(servers)) == 0) {
+    /* If there is no previously committed offset for a partition
          * the auto.offset.reset strategy will be used to decide where
          * in the partition to start fetching messages.
          * By setting this to earliest the consumer will read all messages
          * in the partition if there was no previously committed offset. */
-      /*  if (rd_kafka_conf_set(conf, "auto.offset.reset", "earliest",
+    /*  if (rd_kafka_conf_set(conf, "auto.offset.reset", "earliest",
                               errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK) {
                 MRDERR_<<errstr;
                 rd_kafka_conf_destroy(conf);
         return -2;
     }*/
-    if(rk==NULL){
-      MRDDBG_<<"Consumer apply configuration, groupid:"<<groupid;
-      if(groupid!=""){
-        if(setOption("group.id", groupid.c_str())!=0){
+    if (rk == NULL) {
+      MRDDBG_ << "Consumer apply configuration, groupid:" << groupid;
+      if (groupid != "") {
+        if (setOption("group.id", groupid.c_str()) != 0) {
           return -2;
         }
-      
-    }
-      if(setOption("allow.auto.create.topics","true")!=0){
+      }
+      if (setOption("allow.auto.create.topics", "true") != 0) {
         return -3;
       }
-      rd_kafka_conf_set_error_cb(conf, err_cb);
+      //  rd_kafka_conf_set_error_cb(conf, err_cb);
 
       if (!(rk = rd_kafka_new(RD_KAFKA_CONSUMER, conf, ers, sizeof(ers)))) {
         MRDERR_ << "Failed to create new consumer: " << ers;
-        errstr=ers;
+        errstr = ers;
 
         return -10;
       }
     }
 
-        /* Redirect all messages from per-partition queues to
+    /* Redirect all messages from per-partition queues to
          * the main queue so that messages can be consumed with one
          * call from all assigned partitions.
          *
@@ -145,153 +139,169 @@ int MessagePSRDKafkaConsumer::applyConfiguration() {
          * and each partition queue separately, which requires setting
          * up a rebalance callback and keeping track of the assignment:
          * but that is more complex and typically not recommended. */
-       rd_kafka_poll_set_consumer(rk);
-      
-   
+    rd_kafka_poll_set_consumer(rk);
   }
   return ret;
 }
 
-int MessagePSRDKafkaConsumer::subscribe(const std::string& key){
+int MessagePSRDKafkaConsumer::subscribe(const std::string& key) {
   MessagePSConsumer::subscribe(key);
-  if(rk==NULL){
-    errstr="apply configuration first!";
-     MRDERR_<<errstr;
+  if (rk == NULL) {
+    errstr = "apply configuration first!";
+    MRDERR_ << errstr;
     return -5;
   }
-  rd_kafka_topic_partition_list_t *subscription; 
+  rd_kafka_topic_partition_list_t* subscription;
   subscription = rd_kafka_topic_partition_list_new(keylist.size());
-  for(std::set<std::string>::iterator i=keylist.begin();i!=keylist.end();i++){
-        rd_kafka_topic_partition_list_add(subscription,
-                                                  (*i).c_str(),
-                                                  /* the partition is ignored
+  for (std::set<std::string>::iterator i = keylist.begin(); i != keylist.end(); i++) {
+    rd_kafka_topic_partition_list_add(subscription,
+                                      (*i).c_str(),
+                                      /* the partition is ignored
                                                    * by subscribe() */
-                                                  RD_KAFKA_PARTITION_UA);
-       // MRDDBG_<<" subscribing to "<<*i;
-                                          
-      }
-       rd_kafka_resp_err_t err = rd_kafka_subscribe(rk, subscription);
-        if (err) {
-          MRDERR_<<"Failed to subscribe to "<<subscription->cnt<<" topics, group:\""<<groupid<<"\" err:"<<rd_kafka_err2str(err);
-          errstr=rd_kafka_err2str(err);
-          rd_kafka_topic_partition_list_destroy(subscription);
-          rd_kafka_destroy(rk);
-          return -20;
-        }
-       
-        MRDDBG_<<" subscribing items "<<subscription->cnt;
-      
+                                      RD_KAFKA_PARTITION_UA);
+    // MRDDBG_<<" subscribing to "<<*i;
+  }
+  rd_kafka_resp_err_t err = rd_kafka_subscribe(rk, subscription);
+  if (err) {
+    MRDERR_ << "Failed to subscribe to " << subscription->cnt << " topics, group:\"" << groupid << "\" err:" << rd_kafka_err2str(err);
+    errstr = rd_kafka_err2str(err);
+    rd_kafka_topic_partition_list_destroy(subscription);
+    rd_kafka_destroy(rk);
+    return -20;
+  }
 
-        rd_kafka_topic_partition_list_destroy(subscription);
-        return 0;
+  MRDDBG_ << " subscribing items " << subscription->cnt;
 
+  rd_kafka_topic_partition_list_destroy(subscription);
+  return 0;
 }
 
-void MessagePSRDKafkaConsumer::poll(){
-  rd_kafka_message_t *rkm;
-  if(rk==NULL){
-      MRDERR_ << "Not applied configuration" << errstr;
-      errstr="Not applied configuration";
-      sleep(1);
-      return;
+void MessagePSRDKafkaConsumer::poll() {
+  rd_kafka_message_t* rkm;
+  if (rk == NULL) {
+    MRDERR_ << "Not applied configuration" << errstr;
+    errstr = "Not applied configuration";
+    sleep(1);
+    return;
   }
   rkm = rd_kafka_consumer_poll(rk, 100);
-      if (!rkm)
-              return; /* Timeout: no message within 100ms,
+  if (!rkm)
+    return; /* Timeout: no message within 100ms,
                           *  try again. This short timeout allows
                           *  checking for `run` at frequent intervals.
                           */
 
-      /* consumer_poll() will return either a proper message
+  /* consumer_poll() will return either a proper message
         * or a consumer error (rkm->err is set). */
-       
-      if (rkm->err) {
-              /* Consumer errors are generally to be considered
+
+  if (rkm->err) {
+    /* Consumer errors are generally to be considered
                 * informational as the consumer will automatically
                 * try to recover from all types of errors. */
-        errstr=rd_kafka_message_errstr(rkm);
-        if(rkm->err==RD_KAFKA_RESP_ERR__FATAL){
-          MRDERR_<<"FATAL ERROR error:"<<errstr<<" err:"<<rkm->err;
+    errstr = rd_kafka_message_errstr(rkm);
+    if (rkm->err == RD_KAFKA_RESP_ERR__FATAL) {
+      MRDERR_ << "FATAL ERROR error:" << errstr << " err:" << rkm->err;
 
-        } else {
-          MRDERR_<<"Consumer error:"<<errstr<<" err:"<<rkm->err;
-        }
-        stats.errs++;
-        
-        if(handlers[ONERROR]){
-           ele_t d;
-          d.key=rd_kafka_topic_name(rkm->rkt);
-          d.off=rkm->offset;
-          d.par=rkm->partition;
-          d.cd= chaos::common::data::CDWShrdPtr( new chaos::common::data::CDataWrapper());
-          d.cd->addStringValue("msg",errstr);
-          d.cd->addInt32Value("err",rkm->err);
-          handlers[ONERROR](d);
+    } else {
+      MRDERR_ << "Consumer error:" << errstr << " err:" << rkm->err;
+    }
+    stats.errs++;
 
-        }
+    if (handlers[ONERROR]) {
+      ele_t d;
+      d.key = rd_kafka_topic_name(rkm->rkt);
+      d.off = rkm->offset;
+      d.par = rkm->partition;
+      d.cd  = chaos::common::data::CDWShrdPtr(new chaos::common::data::CDataWrapper());
+      d.cd->addStringValue("msg", errstr);
+      d.cd->addInt32Value("err", rkm->err);
+      handlers[ONERROR](d);
+    }
 
-        rd_kafka_message_destroy(rkm);
-        return;
+    rd_kafka_message_destroy(rkm);
+    return;
+  }
+  stats.counter++;
 
-      }
-      stats.counter++;
+  //  MRDDBG_<<" message from:"<<rd_kafka_topic_name(rkm->rkt)<<" par:"<<rkm->partition<<" off:"<<rkm->offset;
 
-    //  MRDDBG_<<" message from:"<<rd_kafka_topic_name(rkm->rkt)<<" par:"<<rkm->partition<<" off:"<<rkm->offset;
-     
-      /* Print the message key. */
-    /*  if (rkm->key && is_printable(rkm->key, rkm->key_len))
+  /* Print the message key. */
+  /*  if (rkm->key && is_printable(rkm->key, rkm->key_len))
               printf(" Key: %.*s\n",
                       (int)rkm->key_len, (const char *)rkm->key);
       else if (rkm->key)
               printf(" Key: (%d bytes)\n", (int)rkm->key_len);
       */
-        if(rkm->payload && rkm->len){
-        try{
-        //  msgs.push_back(t);
-          if(handlers[ONARRIVE]){
-            ele_t d;
-            d.key=rd_kafka_topic_name(rkm->rkt);
-            d.off=rkm->offset;
-            d.par=rkm->partition;
-            d.cd= chaos::common::data::CDWShrdPtr( new chaos::common::data::CDataWrapper((const char*) rkm->payload, rkm->len));
+  if (rkm->payload && rkm->len) {
+    //  msgs.push_back(t);
+    if (handlers[ONARRIVE]) {
+      ele_t d;
+      d.key = rd_kafka_topic_name(rkm->rkt);
+      d.off = rkm->offset;
+      d.par = rkm->partition;
+      try {
+        d.cd = chaos::common::data::CDWShrdPtr(new chaos::common::data::CDataWrapper((const char*)rkm->payload, rkm->len));
+      } catch (chaos::CException& e) {
+        stats.errs++;
+        MRDERR_ << rkm->offset << "," << rkm->partition << " invalid chaos packet from:" << rd_kafka_topic_name(rkm->rkt) << " len:" << rkm->len << " msg:" << e.what();
+        //<<" string:"<<std::string((const char*)rkm->payload, rkm->len);
+        if (handlers[ONERROR]) {
+          ele_t d;
+          d.key = rd_kafka_topic_name(rkm->rkt);
+          d.off = rkm->offset;
+          d.par = rkm->partition;
+          d.cd  = chaos::common::data::CDWShrdPtr();
 
-            handlers[ONARRIVE](d);
-          } else {
-            ele_t *ele =new ele_t();
-            ele->key=rd_kafka_topic_name(rkm->rkt);
-            ele->off=rkm->offset;
-            ele->par=rkm->partition;
-            ele->cd= chaos::common::data::CDWShrdPtr( new chaos::common::data::CDataWrapper((const char*) rkm->payload, rkm->len));
-            msgs.push(ele);
-            que_elem++;
-            stats.oks++;
-            data_ready=true;
-            cond.notify_all();
-          }
-          stats.oks++;
-        }catch(chaos::CException&e){
-          stats.errs++;
-          MRDERR_<<" invalid chaos packet from:"<<rd_kafka_topic_name(rkm->rkt)<< " len:"<<rkm->len<<" msg:"<<e.what();
-          //<<" string:"<<std::string((const char*)rkm->payload, rkm->len);
-
-        } catch(...){
-          stats.errs++;
-          MRDERR_<<" invalid chaos packet from:"<<rd_kafka_topic_name(rkm->rkt)<< " len:"<<rkm->len;
-          //<<" string:"<<std::string((const char*)rkm->payload, rkm->len);
-
+          handlers[ONERROR](d);
         }
-        } 
-      /* Print the message value/payload. */
-     /* if (rkm->payload && is_printable(rkm->payload, rkm->len))
+        rd_kafka_message_destroy(rkm);
+        return;
+      }
+
+      handlers[ONARRIVE](d);
+    } else {
+      ele_t* ele = new ele_t();
+      ele->key   = rd_kafka_topic_name(rkm->rkt);
+      ele->off   = rkm->offset;
+      ele->par   = rkm->partition;
+
+      try {
+        ele->cd = chaos::common::data::CDWShrdPtr(new chaos::common::data::CDataWrapper((const char*)rkm->payload, rkm->len));
+      } catch (chaos::CException& e) {
+        stats.errs++;
+        MRDERR_ << rkm->offset << "," << rkm->partition << " invalid chaos packet from:" << rd_kafka_topic_name(rkm->rkt) << " len:" << rkm->len << " msg:" << e.what();
+        //<<" string:"<<std::string((const char*)rkm->payload, rkm->len);
+        if (handlers[ONERROR]) {
+          ele_t d;
+          d.key = rd_kafka_topic_name(rkm->rkt);
+          d.off = rkm->offset;
+          d.par = rkm->partition;
+          d.cd  = chaos::common::data::CDWShrdPtr();
+
+          handlers[ONERROR](d);
+        }
+        rd_kafka_message_destroy(rkm);
+        return;
+      }
+      msgs.push(ele);
+      que_elem++;
+      stats.oks++;
+      data_ready = true;
+      cond.notify_all();
+    }
+    stats.oks++;
+  }
+
+  /* Print the message value/payload. */
+  /* if (rkm->payload && is_printable(rkm->payload, rkm->len))
               printf(" Value: %.*s\n",
                       (int)rkm->len, (const char *)rkm->payload);
       else if (rkm->key)
               printf(" Value: (%d bytes)\n", (int)rkm->len);
       */
-      rd_kafka_message_destroy(rkm);
+  rd_kafka_message_destroy(rkm);
 
-}
-
+}  // namespace rdk
 
 }  // namespace rdk
 }  // namespace kafka
